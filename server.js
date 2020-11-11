dad7201bdb85adf73d6847dc3fa9736f3c2dc142
JessicaMoreschi <71377205+JessicaMoreschi@users.noreply.github.com>
2020-11-11 10:58:05 +0100

    create public folder

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 console.log("node is running")
+
+
+// crea local server
+let express = require("express"); //Load the express code
+let app = express(); //create local host
+let port = 3000;  //dichiara server port (3000 standard)
+let server = app.listen(port);  //aspetta che qualcuno si connetta (da browser "localhost:3000")
+                                //per stoppare (ctrl+c); per riavviare "node server.js" (da terminale)
+
+//crea folder per client ("public")
+app.use(express.static("public")); //mostra ai clienti la certella public
